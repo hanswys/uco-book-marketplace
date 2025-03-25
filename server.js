3eb7c2bf0fb68f3a951429b82a428f89640bd327
hanswys <123027447+hanswys@users.noreply.github.com>
2025-03-25 12:56:29 -0500

    added all elements in ui

--- server.js
+++ server.js
@@ -12,5 +12,17 @@ app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.use('/', require('./routes/root'))
 
+//routed to 404.html if not found 
+app.all('*', (req, res) => {
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')){
+        res.json({ message: '404 Not Found'})
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
 //app starts server and console log when successful
 app.listen(PORT, () => console.log(`SErver running on port ${PORT}`))
