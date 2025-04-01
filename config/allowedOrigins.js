241e5ff22ce2235217a87c6e66b2930c99a4fb35
hanswys <123027447+hanswys@users.noreply.github.com>
2025-04-01 00:27:57 -0500

    finished logging, error and cors

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+//with cors allows each url to access our api
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com'
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,35 +1,37 @@
-// get express module
 const express = require('express')
-//creates express app
 const app = express()
-
-// module that helps with file and directory path
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
-
-const { logger } = require('./middleware/logger') 
 
 app.use(logger)
 
-
-//serve static assets from root to /public dir
-app.use('/', express.static(path.join(__dirname, '/public')))
-
-app.use('/', require('./routes/root'))
+//activate cors so unallowed sites will be blocked
+app.use(cors(corsOptions))
 
 app.use(express.json())
 
-//routed to 404.html if not found 
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/', require('./routes/root'))
+
 app.all('*', (req, res) => {
     res.status(404)
-    if(req.accepts('html')){
+    if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if (req.accepts('json')){
-        res.json({ message: '404 Not Found'})
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
     } else {
         res.type('txt').send('404 Not Found')
     }
 })
 
-//app starts server and console log when successful
+app.use(errorHandler)
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
